13c8f7c02d563c1625b6efad88ea62a42c2663e1
Zoe-2688 <zoe26mejiasantana@gmail.com>
2024-06-27 16:57:38 -0400

    Add Cabecera and CabeceraLink components
    - Modify package-lock.json and package.json for dependencies
    - Update Cabecera.js and Cabecera.module.css
    - Add new CabeceraLink.js and CabeceraLink.module.css components

--- src/components/cabecera/Cabecera.js
+++ src/components/cabecera/Cabecera.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./Cabecera.module.css"
-import logo from "./logo-alura.png"
+import logo from "./logo-alura.png";
+import CabeceraLink from "./CabeceraLink/CabeceraLink";
 
 
 function Cabecera() {
@@ -11,6 +12,15 @@ function Cabecera() {
                 <img src={logo} alt="Logo Alura" /> <span>Cinema</span>
             </section>
             </Link>
+            <nav>
+                <CabeceraLink url="./">
+                Home
+                </CabeceraLink>
+
+                <CabeceraLink url="./Favoritos">
+                Favoritos
+                </CabeceraLink>
+            </nav>
         </header>
 )
 }
